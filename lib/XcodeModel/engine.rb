8c257941682a76c8003abba24c6fd7f30b842c53
Yuri Dymov <yuri@dymov.me>
2015-07-12 22:47:47 -0400

    migrate to rails 4 gem

--- lib/XcodeModel/engine.rb
+++ lib/XcodeModel/engine.rb
@@ -1,12 +1,8 @@
 module XcodeModel
   class Engine < Rails::Engine
 
-    initializer "xcode_model.load_app_instance_data" do |app|
-      XcodeModel.setup do |config|
-        config.app_root = app.root
-      end
-    end
-
+    config.app_root = app.root
+    
     initializer "xcode_model.load_static_assets" do |app|
       app.middleware.use ::ActionDispatch::Static, "#{root}/public"
     end
